953464b023d9898b5f8523f4ba72e214536d69c2
dashjay <dashjay@foxmail.com>
2019-09-05 09:24:10 +0800

    fix bugs Optimize the process

--- core/main.py
+++ core/main.py
@@ -1,36 +1,48 @@
 from flask import Flask, request, json, jsonify
 import os
-
+import re
 app = Flask(__name__)
 
+@app.route('/hello',methods=['POST'])
+def hello():
+    return 'ok'
 
 @app.route('/solve', methods=['POST'])
 def judge():
     # 将top_module写入当前目录当中
-    if request.method == 'POST':
-        top_module = request.form['top_module']
-        write_string(top_module, 'top_module.v')
+    form_dict = request.form.to_dict()
 
-        stim = request.form['stim']
-        write_string(stim,'stim.do')
+    if 'top_module' not in form_dict.keys():
+        return json.dumps({'status':0,'msg':'top_module is required'})
 
+    top_module = form_dict['top_module']
+    write_string(top_module, 'top_module.v')
+
+    if 'stim' not in form_dict.keys():
+        return json.dumps({'status':0,'msg':'stim is required'})
+
+    stim = form_dict['stim']
+    write_string(stim,'stim.do')
     #编译
+    os.system('rm cmpresult')
     cmpresult = cmp()
 
+    errornum = int(re.findall('Errors: (.*?),',cmpresult.split('\n')[-2])[0])
+    warningnum = int(re.findall('Warnings: (.*?)$',cmpresult.split('\n')[-2])[0])
+
+    if errornum > 0:
+        return json.dumps({'status': 0, 'cmpcode':0,'cmpresult': cmpresult})
+
     simresult = sim()
-    
     diagram()
-
     signal = try_get('signal.json')
-
     if  signal == "":
         code = 0
     else:
         code = 1
 
-    # os.system('make clean')
-    return json.dumps({'status': code,'comresult': cmpresult, 'signal': signal})
-
+    os.system('make clean')
+    return json.dumps({'status': code, 'cmpcode':1,'cmpresult': cmpresult, 'signal': signal})
 
 def write_string(file_string, filename):
     f = open(filename, 'w')
@@ -88,4 +100,4 @@ def try_get(filename):
         print(e)
         return ""
 
-app.run()
+app.run(host='0.0.0.0',port=5000)
